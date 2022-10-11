34e9c8f8aee4b43dc00e8c1830d75920f3549c39
vyura06 <vechorko.yura@gmail.com>
2022-10-11 14:15:34 +0300

    Update translation

---
+++ client/src/translation/i18next.js
@@ -0,0 +1,26 @@
+import i18n from 'i18next'
+import Backend from 'i18next-http-backend'
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+import { initReactI18next } from 'react-i18next'
+
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: "ru",
+    whitelist: ["ru", "en"],
+    detection: {
+        order:['queryString', 'cookie'],
+        caches:['localStorage', 'cookie']
+    },
+    fallbackLng: 'en',
+    debug: true,
+    interpolation: {
+      escapeValue: false,
+      formatSeparator: ','
+    }
+  })
+
+export default i18n
