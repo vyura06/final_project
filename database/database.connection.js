23696bdc81b0ac09d57f1278126a8cfdac9b7e77
vyura06 <vechorko.yura@gmail.com>
2022-10-14 10:58:09 +0300

    Update info for db

--- database/database.connection.js
+++ database/database.connection.js
@@ -1,8 +1,8 @@
 module.exports = {
-    host: "ec2-52-48-159-67.eu-west-1.compute.amazonaws.com",
-    user: "ktfulspejaiilw",
-    pass: "4931e09acc03ccae82bb6c2c58a1e03391721e92c3f673d64fd9826c544dbb48",
-    dbname: "d33jqos7hgdc7",
+    host: "ec2-99-80-170-190.eu-west-1.compute.amazonaws.com",
+    user: "rsxovomonpwnwi",
+    pass: "54153c02659ad8d51da9041c2a38c52e9cc7ad5a6e7a047dca24b9fa7dbc3a41",
+    dbname: "d423bpbk3tt99k",
     port: 5432,
     ssl: { rejectUnauthorized: false }
   };
